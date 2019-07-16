daec627b7089bb453dadd24319c991a236a02556
Jarod Peachey <jwpeachey107@aol.com>
2019-07-16 10:20:14 -0400

    Update file structure

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import ContactsList from '../ContactsList';
+import ContactsList from '../Contacts/ContactsList';
 
 class Main extends Component {
   static propTypes = {
