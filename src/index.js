f488f90fa054ef18d309b5ff347a9673752e80e5
Jarod Peachey <jwpeachey107@aol.com>
2019-07-18 15:57:48 -0400

    Latest working version

--- src/index.js
+++ src/index.js
@@ -2,24 +2,35 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {
-  browserHistory, BrowserRouter,
+  browserHistory,
+  Router,
+  Route,
+  IndexRedirect,
 } from 'react-router';
-// import { useScroll } from 'react-router-scroll';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { ThemeProvider } from 'styled-components';
 import muiTheme from './mui-theme';
 import styledTheme from './styled-theme';
-import routes from './Routes';
+import Application from './Application';
+import Main from './components/pages/Main';
+// import { useScroll } from 'react-router-scroll';
 
 function startApp () {
   render(
     <MuiThemeProvider theme={muiTheme}>
       <ThemeProvider theme={styledTheme}>
-        <BrowserRouter
+        <Router
           history={browserHistory}
         >
-          {routes()}
-        </BrowserRouter>
+          <Route path="/" component={Application}>
+            {
+            (function runFunction () {
+              return <IndexRedirect to="/welcome" />;
+            }())
+            }
+            <Route path="/welcome" component={Main} />
+          </Route>
+        </Router>
       </ThemeProvider>
     </MuiThemeProvider>, document.getElementById('app'),
   );
