a9cbca897b3b82461d3637d1b9f1bfe6d2b6e09e
Jarod Peachey <jwpeachey107@aol.com>
2019-07-19 15:58:22 -0400

    Update and include redux

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import { MuiThemeProvider } from '@material-ui/core/styles';
@@ -7,7 +6,6 @@ import { ThemeProvider } from 'styled-components';
 import muiTheme from './mui-theme';
 import styledTheme from './styled-theme';
 // import styled from 'styled-components';
-import store from './Store';
 import Header from './components/Header';
 import Main from './components/pages/Main';
 
@@ -35,20 +33,18 @@ class Application extends Component {
     const header = <Header />;
 
     return (
-      <Provider store={store}>
-        <MuiThemeProvider theme={muiTheme}>
-          <ThemeProvider theme={styledTheme}>
-            <Router>
-              <div className="app">
-                {header}
-                <Switch>
-                  <Route exact path="/" component={Main} />
-                </Switch>
-              </div>
-            </Router>
-          </ThemeProvider>
-        </MuiThemeProvider>
-      </Provider>
+      <MuiThemeProvider theme={muiTheme}>
+        <ThemeProvider theme={styledTheme}>
+          <Router>
+            <div className="app">
+              {header}
+              <Switch>
+                <Route exact path="/" component={Main} />
+              </Switch>
+            </div>
+          </Router>
+        </ThemeProvider>
+      </MuiThemeProvider>
     );
   }
 }
