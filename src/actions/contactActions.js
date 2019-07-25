aa9f4a9024f56b64d5e9f615ef048d49ab540bc4
Jarod Peachey <jwpeachey107@aol.com>
2019-07-25 12:56:31 -0400

    Update to add contact to api

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -3,13 +3,29 @@ import 'babel-polyfill';
 import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from './types';
 
 export const getContacts = () => async (dispatch) => {
-  const res = await axios.get('https://my-json-server.typicode.com/jarodpeachey/react-contact-manager-api/contacts');
+  const res = await axios.get('https://contactmanager-api.herokuapp.com/contacts');
   dispatch({
     type: GET_CONTACTS,
     payload: res.data,
   });
 };
 
-export const addContact = (id, name, email, phone) => ({ type: ADD_CONTACT, payload: { id, name, email, phone } });
+export const addContact = (id, name, email, phone) => async (dispatch) => {
+  const contact = {
+    id,
+    name,
+    email,
+    phone,
+  };
+
+  axios.post('https://contactmanager-api.herokuapp.com/contacts', contact)
+    .then((res) => { console.log(res); })
+    .catch((err) => { console.log(err); });
+
+  dispatch({
+    type: ADD_CONTACT,
+    payload: contact,
+  });
+};
 
 export const deleteContact = id => ({ type: DELETE_CONTACT, payload: id });
